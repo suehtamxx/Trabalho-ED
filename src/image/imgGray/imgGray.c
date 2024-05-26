501b4c113fa9c33c76d30d2158830399e6fd93db
rayss4lves <rayssasantos.com890@gmail.com>
2024-05-26 14:54:22 -0300

    alterações imageGray

--- src/image/imgGray/imgGray.c
+++ src/image/imgGray/imgGray.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 
 ImageGray *create_image_gray(int largura, int altura){
-  
+  ImageGray *imggray = (ImageGray*)malloc(sizeof(ImageGray));
+  imggray->dim.altura = altura;
+  imggray->dim.largura = largura;
+  //imggray->pixels 
 }
 
 void helloWord()
