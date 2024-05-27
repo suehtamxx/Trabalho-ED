ecaa664f55364c291e1a23080b51679aab05b0f2
KauaHenSilva <kaua.sbc@gmail.com>
2024-05-27 12:49:11 -0300

    fix: imgVert

--- main.c
+++ main.c
@@ -2,10 +2,6 @@
 
 int main()
 {
-  ImageRGB *create_image_rgb(int largura, int altura);
-  void free_image_rgb(ImageRGB *image);
-  ImageRGB *flip_vertical_rgb(const ImageRGB *image);
-  ImageRGB *flip_horizontal_rgb(const ImageRGB *image);
-  //start(0, NULL); 
+  start(0, NULL); 
   return 0;
 }
