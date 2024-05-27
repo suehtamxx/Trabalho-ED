9498b5890b585d33e8b3aa199abdc08a02f563d1
Rayssa Alves <129634549+rayss4lves@users.noreply.github.com>
2024-05-26 22:35:20 -0300

    Update imgGray.c
    add: read_image_gray

--- src/image/imgGray/imgGray.c
+++ src/image/imgGray/imgGray.c
@@ -1,5 +1,6 @@
 #include <imgGray.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 ImageGray *create_image_gray(int largura, int altura)
 {
@@ -16,6 +17,46 @@ void free_image_gray(ImageGray *image)
   {
     free(image->pixels);
     free(image);
+  }
+}
+
+ImageGray *read_image_gray_from_file(const char *filename)
+{
+  FILE *file = fopen(filename, "r");
+  if (!file)
+  {
+    fprintf(stderr, "Não foi possível abrir o arquivo %s\n", filename);
+    return NULL;
+  }
+
+  int largura, altura;
+  // Leia as dimensões da imagem
+  if (fscanf(file, "%d %d", &largura, &altura) != 2)
+  {
+    fprintf(stderr, "Falha ao ler as dimensões da imagem\n");
+    fclose(file);
+    return NULL;
+  }
+
+  ImageGray *image = create_image_gray(largura, altura);
+  if (!image)
+  {
+    fclose(file);
+    return NULL;
+  }
+
+  for (int i = 0; i < altura; i++)
+  {
+    for (int j = 0; j < largura; j++)
+    {
+      if (fscanf(file, "%d", &image->pixels[i * largura + j].value) != 1)
+      {
+        fprintf(stderr, "Falha ao ler o valor do pixel em (%d, %d)\n", i, j);
+        free_image_gray(image);
+        fclose(file);
+        return NULL;
+      }
+    }
   }
 }
 
