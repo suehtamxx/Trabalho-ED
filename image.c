dc0f1fb49b4f56a40efa370bbb8a5c23293a21ab
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-06-03 17:38:42 -0300

    Update image.c

--- image.c
+++ image.c
@@ -7,8 +7,41 @@ ImageGray *create_image_gray(int largura, int altura)
 {
     ImageGray *imageGray = malloc(sizeof(imageGray));
 
+    //Criando arquivo
+    FILE *arqGray;
+    arqGray = fopen("imageGray.txt", "r");
+    if (arqGray == NULL)
+    {
+        printf("ERRO ao abrir o arquivo!\n");
+        fclose(arqGray);
+        exit(1);
+    }
+
     //Alocando memoria
-    
+    imageGray->pixels = (PixelGray*)malloc((largura * altura) * sizeof(PixelGray));
+    if (imageGray->pixels == NULL)
+    {
+        printf("ERRO ao alocar pixels!\n");
+        exit(1);
+    }
+
+    //Lendo do arquivo e colocando na struct
+    for (int i = 0; i < altura * largura; i++)
+    {
+        int pixel;
+        if (fscanf(arqGray, "%d", &pixel) != 1)
+        {
+            printf("ERRO ao ler pixel.\n");
+            free(imageGray->pixels);
+            free(imageGray);
+            exit(1);
+        }
+
+        imageGray->pixels[i].value = pixel;
+        printf("%d ", imageGray->pixels[i].value);        
+    }
+
+    fclose(arqGray);
     
     return imageGray;
 }
