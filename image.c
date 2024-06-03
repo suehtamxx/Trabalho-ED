fa9542a96a6780e111878d60ae1e24268b790788
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-06-03 16:15:16 -0300

    Update image.c

--- image.c
+++ image.c
@@ -7,18 +7,7 @@ ImageGray *create_image_gray(int largura, int altura)
 {
     ImageGray *image;
 
-    //Criando arquivo
-    FILE *arqGray;
-    arqGray = fopen("input_image_example_RGB.txt", "r");
-    if (arqGray == NULL)
-    {
-        printf("ERRO ao abrir o arquivo!\n");
-        return;
-    }
-
-    //Retirando as dimensoes
-    fscanf(arqGray, "%d", &image->dim.largura);
-    fscanf(arqGray, "%d", &image->dim.altura);
+    
     
 
     //Alocando memoria
