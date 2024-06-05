237da2f933cfa6f02ff8f68df4d5b6be30ab8165
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-06-04 23:58:21 -0300

    Update image.c

--- image.c
+++ image.c
@@ -3,40 +3,8 @@
 #include <stdlib.h>
 
 // Ler arquivos
-void readFileGray()
-{
-    ImageGray *imageGray = malloc(sizeof(ImageGray));
-
-    //Criando arquivo
-    FILE *arqGray;
-    arqGray = fopen("imageGray.txt", "r");
-    if (arqGray == NULL)
-    {
-        printf("ERRO ao abrir o arquivo!\n");
-        fclose(arqGray);
-        exit(1);
-    }
-
-    if (fscanf(arqGray,"%d %d", &imageGray->dim.largura, &imageGray->dim.altura) !=2)
-    {
-        printf("erro na leitura da dimensão");
-        fclose(arqGray);
-        free(imageGray);
-        exit(1);
-    }
-
-    fgetc(arqGray);
-
-    //Alocando memoria
-    imageGray->pixels = malloc(imageGray->dim.largura * imageGray->dim.altura * sizeof(PixelGray));
-    if (imageGray->pixels == NULL)
-    {
-        printf("ERRO ao alocar pixels!\n");
-        fclose(arqGray);
-        free(imageGray);
-        exit(1);
-    }
-
+void readFileGray(ImageGray *imageGray, FILE *arqGray)
+{ 
     //Lendo os pixels
     for(int i = 0; i < imageGray->dim.largura * imageGray->dim.altura; i++)
     {
@@ -53,66 +21,218 @@ void readFileGray()
 
         //Atribuindo o valor do pixel
         imageGray->pixels[i].value = pixel;
-        printf("%d ", imageGray->pixels[i].value);   
+        //printf("%d ", imageGray->pixels[i].value);   
     }
-
-    fclose(arqGray);
 }
-
-void readFileRGB()
+void readFileRGB(ImageRGB *imageRGB, FILE *arqRGB)
 {
-    ImageRGB *imageRGB = malloc(sizeof(ImageRGB));
-
-    //Criando arquivo
-    FILE *arqRGB;
-    arqRGB = fopen("imageRGB.txt", "r");
-    if (arqRGB == NULL)
-    {
-        printf("ERRO ao abrir o arquivo!\n");
-        fclose(arqRGB);
-        exit(1);
-    }
-    if (fscanf(arqRGB,"%d %d", &imageRGB->dim.largura, &imageRGB->dim.altura) !=2)
-    {
-        printf("erro na leitura da dimensão");
-        fclose(arqRGB);
-        free(imageRGB);
-        exit(1);
-    }
-
-    fgetc(arqRGB);
-
-    //Alocando memoria
-    imageRGB->pixels = malloc(imageRGB->dim.largura * imageRGB->dim.altura * sizeof(PixelRGB));
-    if (imageRGB->pixels == NULL)
-    {
-        printf("ERRO ao alocar pixels!\n");
-        fclose(arqRGB);
-        free(imageRGB);
-        exit(1);
-    }
-
     //Lendo os pixels
-    for(int i = 0; i < imageRGB->dim.largura * imageRGB->dim.altura; i++)
+    for(int i = 0; i < (imageRGB->dim.largura * imageRGB->dim.altura); i++)
     {
         int r, g, b;
         char separador;
         if(fscanf(arqRGB, "%d %d %d%c", &r, &g, &b, &separador) != 4)
         {
-            printf("erro na leitura do pixel\n");
-            fclose(arqRGB);
+            printf("ERRO na leitura do pixel\n");
             free(imageRGB->pixels);
             free(imageRGB);
+            fclose(arqRGB);
             exit(1);
         }
-
+        
+        //Atribuindo o valor do pixel
         imageRGB->pixels[i].red = r;
         imageRGB->pixels[i].green = g;
         imageRGB->pixels[i].blue = b;
-        printf("%d %d %d ", imageRGB->pixels[i].red, imageRGB->pixels[i].green, imageRGB->pixels[i].blue);   
+        //printf("%d %d %d ", imageRGB->pixels[i].red, imageRGB->pixels[i].green, imageRGB->pixels[i].blue);   
+    }
+}
+
+// Funções de criação e liberação
+ImageGray *create_image_gray(int largura, int altura)
+{
+    //Alocando struct
+    ImageGray *image = malloc(sizeof(ImageGray));
+    if (image == NULL)
+    {
+        printf("ERRO ao alocar struct gray!\n");
+        exit(1);
     }
 
+    //Atribuindo os valores a struct
+    image->dim.largura = largura;
+    image->dim.altura = altura;
+    //printf("Dimensoes: %d %d", image->dim.largura, image->dim.altura);
+
+    //Alocanco os pixels
+    image->pixels = malloc((largura * altura) * sizeof(PixelGray));
+    if (image->pixels == NULL)
+    {
+        printf("ERRO ao alocar pixels da struct gray!\n");
+        free(image);
+        exit(1);
+    }
+    
+    return image;
+}
+void free_image_gray(ImageGray *image)
+{
+    free(image->pixels);
+    free(image);
+}
+
+ImageRGB *create_image_rgb(int largura, int altura)
+{
+    //Alocando struct
+    ImageRGB *image = malloc(sizeof(ImageRGB));
+    if (image == NULL)
+    {
+        printf("ERRO ao alocar struct rgb!\n");
+        exit(1);
+    }
+
+    //Atribuindo os valores a struct
+    image->dim.largura = largura;
+    image->dim.altura = altura;
+    //printf("Dimensoes: %d %d", image->dim.largura, image->dim.altura);
+
+    //Alocanco os pixels
+    image->pixels = malloc((largura * altura) * sizeof(PixelRGB));
+    if (image->pixels == NULL)
+    {
+        printf("ERRO ao alocar pixels da struct rgb!\n");
+        free(image);
+        exit(1);
+    }
+    
+    return image;
+}
+void free_image_rgb(ImageRGB *image)
+{
+    free(image->pixels);
+    free(image);
+}
+
+// Converter struct para txt
+void convertGraytxt(ImageGray *image)
+{
+    //Criando o arquivo
+    FILE *arqGray;
+    arqGray = fopen("transposeGray.txt", "w");
+    if (arqGray == NULL)
+    {
+        printf("ERRO ao criar arquivo.\n");
+        exit(1);
+    }
+
+    //Escrevendo as dimensões no arquivo
+    fprintf(arqGray, "%d\n", image->dim.largura);
+    fprintf(arqGray, "%d\n", image->dim.altura);
+    
+    //Escrevendo os pixels no arquivo
+    for (int i = 0; i < image->dim.altura; i++)
+    {
+        for (int j = 0; j < image->dim.largura; j++)
+        {
+            if (j == image->dim.largura - 1) fprintf(arqGray, "%d,\n", image->pixels[i * image->dim.largura + j].value);
+            else fprintf(arqGray, "%d, ", image->pixels[i * image->dim.largura + j].value);
+        }
+    }
+    
+    fclose(arqGray);
+}
+void convertRGBtxt(ImageRGB *image)
+{
+    //Criando o arquivo
+    FILE *arqRGB;
+    arqRGB = fopen("transposeRGB.txt", "w");
+    if (arqRGB == NULL) 
+    {
+        printf("ERRO ao criar arquivo.\n");
+        exit(1);
+    }
+
+    //Escrevendo as dimensões no arquivo
+    fprintf(arqRGB, "%d\n", image->dim.largura);
+    fprintf(arqRGB, "%d\n", image->dim.altura);
+    
+    //Escrevendo os pixels no arquivo
+    for (int i = 0; i < image->dim.altura; i++)
+    {
+        for (int j = 0; j < image->dim.largura; j++)
+        {
+            if (j == image->dim.largura - 1) fprintf(arqRGB, "%d %d %d,\n", image->pixels[i * image->dim.largura + j].red, image->pixels[i * image->dim.largura + j].green, image->pixels[i * image->dim.largura + j].blue);
+            else fprintf(arqRGB, "%d %d %d, ", image->pixels[i * image->dim.largura + j].red, image->pixels[i * image->dim.largura + j].green, image->pixels[i * image->dim.largura + j].blue);
+        }
+    }
+    
     fclose(arqRGB);
 }
 
+// Operações para ImageGray
+ImageGray *transpose_gray(const ImageGray *image)
+{
+    ImageGray *image_transpose = malloc(sizeof(ImageGray));
+    if(image_transpose == NULL)
+    {
+        printf("ERRO ao alocar trasnpose gray!");
+        exit(1);
+    }
 
+    image_transpose->dim.largura = image->dim.altura;
+    image_transpose->dim.altura = image->dim.largura;
+    //printf("Dimensoes: %d %d", image_transpose->dim.largura, image_transpose->dim.altura);
+
+    image_transpose->pixels = malloc(image_transpose->dim.altura * image_transpose->dim.largura * sizeof(PixelGray));
+    if(image_transpose->pixels == NULL)
+    {
+        printf("ERRO ao alocar pixels transpose gray!");
+        free(image_transpose);
+        exit(1);
+    }
+
+    for(int i = 0; i < image_transpose->dim.altura; i++)
+    {
+        for(int j = 0; j < image_transpose->dim.largura; j++)
+        {
+            image_transpose->pixels[i * image_transpose->dim.largura + j] = image->pixels[j * image->dim.altura + i];
+            //printf("%d ", image_transpose->pixels[i * image->dim.largura + j].value);
+        }    
+    }
+
+    return image_transpose;
+}
+
+// Operações para ImageRGB
+ImageRGB *transpose_rgb(const ImageRGB *image)
+{
+    ImageRGB *image_transpose = malloc(sizeof(ImageRGB));
+    if(image_transpose == NULL)
+    {
+        printf("ERRO ao alocar trasnpose rgb!");
+        exit(1);
+    }
+
+    image_transpose->dim.largura = image->dim.altura;
+    image_transpose->dim.altura = image->dim.largura;
+    //printf("Dimensoes: %d %d", image_transpose->dim.largura, image_transpose->dim.altura);
+
+    image_transpose->pixels = malloc(image_transpose->dim.altura * image_transpose->dim.largura * sizeof(PixelGray));
+    if(image_transpose->pixels == NULL)
+    {
+        printf("ERRO ao alocar pixels transpose rgb!");
+        free(image_transpose);
+        exit(1);
+    }
+
+    for(int i = 0; i < image_transpose->dim.altura; i++)
+    {
+        for(int j = 0; j < image_transpose->dim.largura; j++)
+        {
+            image_transpose->pixels[i * image_transpose->dim.largura + j] = image->pixels[j * image->dim.altura + i];
+            //printf("%d %d %d ", image->pixels[i * image->dim.largura + j].red, image->pixels[i * image->dim.largura + j].green, image->pixels[i * image->dim.largura + j].blue);
+        }    
+    }
+
+    return image_transpose;
+}
