ba9707b94066edfee0380585411bf0fc28c2f35c
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-06-07 17:06:44 -0300

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -67,5 +67,5 @@ def image_rgb_from_txt(txt_path, output_path):
 # image_gray_from_txt("AlteracaoGray3.txt", "AlteracaoGray3.png")
 # image_rgb_from_txt("AlteracaoRGB3.txt", "AlteracaoRGB3.png")
 
-# image_gray_from_txt("AlteracaoGray4.txt", "AlteracaoGray4.png")
-image_rgb_from_txt("AlteracaoRGB4.txt", "AlteracaoRGB4.png")
+image_gray_from_txt("AlteracaoGray4.txt", "AlteracaoGray4.png")
+# image_rgb_from_txt("AlteracaoRGB4.txt", "AlteracaoRGB4.png")
