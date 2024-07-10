d602b918ae12e2589402cfd244d37539a18057f3
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-07-10 17:56:34 -0300

    Update image.h

--- image.h
+++ image.h
@@ -38,12 +38,14 @@ typedef struct listagray{
     struct listagray *prox;
     struct listagray *ant;
     ImageGray *image;
+    char *nome;
 } Listagray;
 
 typedef struct listargb{
     struct listargb *prox;
     struct listargb *ant;
     ImageRGB *image;
+    char *nome;
 } Listargb;
 
 typedef struct{
@@ -76,7 +78,7 @@ typedef struct elementorgb
 void readFileGray();
 void readFileRGB();
 
-// Funções de criação e liberação
+// Funções de criação e liberação de struct
 ImageGray *create_image_gray(int largura, int altura);
 void free_image_gray(ImageGray *image);
 
@@ -84,8 +86,8 @@ ImageRGB *create_image_rgb(int largura, int altura);
 void free_image_rgb(ImageRGB *image);
 
 // Converter struct para txt
-void convertGraytxt(ImageGray *image, int *numAlteracoes);
-void convertRGBtxt(ImageRGB *image, int *numAlteracoes);
+void convertGraytxt(ImageGray *image, int *numAlteracoes, char *nome, LinkedGray *l);
+void convertRGBtxt(ImageRGB *image, int *numAlteracoes, char *nome, LinkedRGB *l);
 void convertGrayRandomtxt(ImageGray *image, int *numAlteracoes, ListRandomGray **l);
 void convertRGBRandomtxt(ImageRGB *image, int *numAlteracoes, ListRandomRGB **l);
 
@@ -93,47 +95,50 @@ void convertRGBRandomtxt(ImageRGB *image, int *numAlteracoes, ListRandomRGB **l)
 ImageGray *flip_vertical_gray(ImageGray *image);
 ImageGray *flip_horizontal_gray(ImageGray *image);
 ImageGray *transpose_gray(const ImageGray *image);
+ImageGray *clahe_gray(const ImageGray *image, int tile_width, int tile_height);
+ImageGray *median_blur_gray(const ImageGray *image, int kernel_size);
+ImageGray *random_gray(ImageGray *image);
 
 // Operações para ImageRGB
 ImageRGB *flip_vertical_rgb(const ImageRGB *image);
 ImageRGB *flip_horizontal_rgb(const ImageRGB *image);
 ImageRGB *transpose_rgb(const ImageRGB *image);
-
-// Manipulação por pixel para ImageGray
-ImageGray *clahe_gray(const ImageGray *image, int tile_width, int tile_height);
-ImageGray *median_blur_gray(const ImageGray *image, int kernel_size);
-
-// Manipulação por pixel para ImageRGB
 ImageRGB *clahe_rgb(const ImageRGB *image, int tile_width, int tile_height);
 ImageRGB *median_blur_rgb(const ImageRGB *image, int kernel_size);
-
-ImageGray *random_gray(ImageGray *image);
 ImageRGB *random_rgb(ImageRGB *image);
 
+//Operações da Lista simples
 ListRandomGray *create_list_random_gray();
-ListRandomRGB *create_list_random_RGB();
-
 void add_list_random_gray(ListRandomGray **l, ImageGray *image, char *nome);
-void add_list_random_rgb(ListRandomRGB **l, ImageRGB *image, char *nome);
-
 void print_list_random_gray(ListRandomGray *l);
-void print_list_random_rgb(ListRandomRGB *l);
-
 void free_random_gray(ListRandomGray *l);
+
+ListRandomRGB *create_list_random_RGB();
+void add_list_random_rgb(ListRandomRGB **l, ImageRGB *image, char *nome);
+void print_list_random_rgb(ListRandomRGB *l);
 void free_random_rgb(ListRandomRGB *l);
 
-LinkedRGB *criar_RGB();
+//Operação da Lista dupla
 LinkedGray *criar_gray();
-
-void adicionar_rgb(LinkedRGB *l, ImageRGB *image);
-void adicionar_gray(LinkedGray *l, ImageGray *image);
-
-void liberar_rgb(LinkedRGB *l);
+void adicionar_gray(LinkedGray *l, ImageGray *image, char *nome);
 void liberar_gray(LinkedGray *l);
-
 ImageGray *desfazer_gray(LinkedGray *l);
-ImageRGB *desfazer_rgb(LinkedRGB *l);
 ImageGray *refazer_gray(LinkedGray *l);
+
+LinkedRGB *criar_RGB();
+void adicionar_rgb(LinkedRGB *l, ImageRGB *image, char *nome);
+void liberar_rgb(LinkedRGB *l);
+ImageRGB *desfazer_rgb(LinkedRGB *l);
 ImageRGB *refazer_rgb(LinkedRGB *l);
+
+//Chamada do python
+void call_python_gray(int *numAlteracoes, char *nome);
+void call_python_rgb(int *numAlteracoes, char *nome);
+
+void call_python_historico_gray(char *nome);
+void call_python_historico_rgb(char *nome);
+
+void call_python_gray_random(int *numAlteracoes);
+void call_python_rgb_random(int *numAlteracoes);
 
 #endif // IMAGE_H
