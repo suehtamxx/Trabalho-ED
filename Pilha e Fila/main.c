2a49f8f3a6555a9f56b31268aed2eb672fe141e5
VivySilva <77069795+VivySilva@users.noreply.github.com>
2024-07-28 22:42:19 -0300

    Update main.c

--- Pilha e Fila/main.c
+++ Pilha e Fila/main.c
@@ -1,1 +1,19 @@
 //Não esquecer de criar o score
+#include "transportadora.h"
+#include "transportadora.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main ()
+{
+    int id = 0;
+    ListaCliente *listaCliente = criarLista();
+
+    addLista(listaCliente, &id);
+    addLista(listaCliente, &id);
+    mostrarLista(listaCliente);
+    liberarLista(listaCliente);
+
+    return 0;
+}
